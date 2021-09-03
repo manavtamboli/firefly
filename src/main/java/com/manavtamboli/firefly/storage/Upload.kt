a54884ea3b88c131a68c233b57e5ec3f08fdd2ea
manavtamboli <manavtamboli8@gmail.com>
2021-09-03 15:58:32 +0530

    Storage - Simple upload and download added.

--- src/main/java/com/manavtamboli/firefly/storage/Upload.kt
+++ src/main/java/com/manavtamboli/firefly/storage/Upload.kt
@@ -1,21 +1,37 @@
+@file:Suppress("unused")
+
 package com.manavtamboli.firefly.storage
 
+import android.net.Uri
 import com.google.firebase.storage.StorageMetadata
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
 import com.google.firebase.storage.UploadTask
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.io.InputStream
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-suspend fun StorageReference.upload(stream : InputStream) = suspendCancellableCoroutine<UploadTask.TaskSnapshot> { continuation ->
-    putStream(stream)
-        .addOnSuccessListener { continuation.resume(it) }
-        .addOnFailureListener { continuation.resumeWithException(it) }
-}
+suspend fun StorageReference.upload(stream: InputStream) = upload { putStream(stream) }
 
-suspend fun StorageReference.upload(stream : InputStream, metaData : StorageMetadata) = suspendCancellableCoroutine<UploadTask.TaskSnapshot> { continuation ->
-    putStream(stream, metaData)
+suspend fun StorageReference.upload(fileUri : Uri) = upload { putFile(fileUri) }
+
+suspend fun StorageReference.upload(bytes : ByteArray) = upload { putBytes(bytes) }
+
+suspend fun StorageReference.upload(stream: InputStream, metadata: StorageMetadata) = upload { putStream(stream, metadata) }
+
+suspend fun StorageReference.upload(fileUri: Uri, metadata: StorageMetadata) = upload { putFile(fileUri, metadata) }
+
+suspend fun StorageReference.upload(bytes: ByteArray, metadata: StorageMetadata) = upload { putBytes(bytes, metadata) }
+
+private suspend fun upload(taskProducer : () -> StorageTask<UploadTask.TaskSnapshot>) = suspendCancellableCoroutine<UploadTask.TaskSnapshot> { continuation ->
+    taskProducer()
         .addOnSuccessListener { continuation.resume(it) }
         .addOnFailureListener { continuation.resumeWithException(it) }
 }
